cd35c1dda1cfa75110d1f5ea1610df3492c66c2e
jieun <jein6316@gmail.com>
2024-12-01 13:22:54 +0900

    환경변수수정

--- app/api/auth/auth.ts
+++ app/api/auth/auth.ts
@@ -1,38 +1,27 @@
 import axios from 'axios';
-import { LoginResponseData, HandleSubmitParams } from '@/app/types/apiTypes';
+import { useMutation } from '@tanstack/react-query';
+import { LoginResponseData,LoginFormData, HandleSubmitParams } from '@/app/types/apiTypes';
 
 //로그인
-export const loginSubmit = async ({
-    e,
-    formData,
-}: HandleSubmitParams) => {
-    e.preventDefault();  // 폼 제출 시 새로 고침을 막음
-
+// 로그인 요청 함수
+export const loginSubmit = async (formData: LoginFormData): Promise<LoginResponseData> => {
     try {
-        const res = await axios.post<LoginResponseData>(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,  // 엔드포인트 URL
-            formData,  // FormData 객체를 전송
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.NEXT_PUBLIC_LOGIN_TOKEN}`,  // Authorization 헤더에 Bearer 토큰 포함                
-                    'Content-Type': 'application/json',  // 요청 헤더에 Content-Type 설정
-                }
-            }
-        );
-
-        // 로그인 성공 처리
-        const data = res.data;
-        console.log('로그인 성공:', data);
-
-        // 로그인 성공 후 추가 작업 (예: 토큰 저장, 페이지 이동 등)
-        localStorage.setItem('accessToken', data.accessToken);
-        localStorage.setItem('refreshToken', data.refreshToken);
-
-        return { success: true, data }; // 성공 시 data 포함 반환
-    } catch (error: any) {
-        return { success: false, error: error }; // 실패 시 에러 메시지 포함 반환
+        debugger;
+      const res = await axios.post<LoginResponseData>(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      return res.data; // 서버 응답 데이터 반환
+    } catch (error:any) {
+      // 요청이 실패하면 에러를 처리
+      throw new Error(error.response?.data?.message || 'Login failed');
     }
-};
+  };
 
 //회원가입
 export const registerSubmit = async ({
@@ -66,14 +55,12 @@ export const registerSubmit = async ({
 
 //아이디중복체크
 export const checkDuplicateIdAPI = async (userId: string) => {
+    debugger;
     try {
-        const res = await axios.post(
+        const res = await axios.get(
             `${process.env.NEXT_PUBLIC_API_URL}/api/auth/check-duplicate-id`,
-            { userId }, // 요청 데이터에 userId 포함
             {
-                headers: {
-                    'Content-Type': 'application/json', // 요청 데이터 형식에 맞는 헤더 설정
-                },
+                params: { userId }, // GET 요청의 쿼리 파라미터로 전달
             }
         );
 
