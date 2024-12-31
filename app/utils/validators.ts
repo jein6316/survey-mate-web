d6e9203e3fcb1c9c058f56569a6268e097575b5d
jieun <jein6316@gmail.com>
2024-12-31 16:06:42 +0900

    20241231

--- app/utils/validators.ts
+++ app/utils/validators.ts
@@ -1,1 +1,18 @@
 /*입력값 검증 함수*/
+
+
+declare global {
+    interface Window {
+      isNullOrEmpty: typeof isNullOrEmpty;
+      isInvalidFileType: typeof isInvalidFileType;
+    }
+  }
+//문자열 검증
+export const isNullOrEmpty = (value: string | null | undefined): boolean => {
+    return value === null || value === undefined || value.trim() === '';
+  };
+
+//파일형식 검증
+export const isInvalidFileType = (fileType: File |string | null | undefined): boolean => {
+ return fileType === null || fileType === undefined ;
+};
