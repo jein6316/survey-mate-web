125702db57914df78ac914a5e3ad7c371608e227
jieun <jein6316@gmail.com>
2025-01-12 15:39:35 +0900

    20250112

--- app/component/form/LoginForm.tsx
+++ app/component/form/LoginForm.tsx
@@ -4,14 +4,17 @@ import React, { useState } from "react";
 import { LoginFormData, APIResponse } from "@/app/types/apiTypes";
 import { loginSubmit } from "@/app/api/auth/auth";
 import useMutationLogin from "@/app/hooks/useMutationLogin";
-
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { SubmitButton } from "@/app/component/button/Submit-button";
 export function LoginForm({
   action,
   children,
 }: {
   action?: any;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }) {
+  const router = useRouter(); // useRouter 훅 사용
   //로그인 폼데이터
   const [formData, setFormData] = useState<LoginFormData>({
     userId: "",
@@ -42,55 +45,91 @@ export function LoginForm({
   };
 
   return (
-    <form
-      action={action}
-      onSubmit={handleSubmit}
-      className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 sm:px-16"
-    >
-      <div>
-        <label
-          htmlFor="userId"
-          className="block text-xs text-gray-600 uppercase"
+    <div className="flex min-h-screen w-screen items-center justify-center bg-gray-50 px-4">
+      <div className="z-10 w-full max-w-md overflow-hidden rounded-lg border border-gray-300 shadow-lg bg-white">
+        {/* 헤더 */}
+        <header className="flex flex-col items-center justify-center space-y-2 border-b border-gray-200 px-6 py-8 text-center">
+          <h1 className="text-3xl font-extrabold text-gray-900">SURVEY MATE</h1>
+        </header>
+
+        {/* 로그인 폼 */}
+        <form
+          action={action}
+          onSubmit={handleSubmit}
+          className="flex flex-col space-y-4 bg-gray-50 px-6 py-8 sm:px-16"
         >
-          userId
-        </label>
-        <input
-          id="userId"
-          name="userId"
-          type="text"
-          placeholder="userId"
-          autoComplete="userId"
-          onChange={handleChange}
-          value={formData.userId}
-          required
-          className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-        />
+          <div>
+            <label
+              htmlFor="userId"
+              className="block text-xs text-gray-600 uppercase"
+            >
+              User ID
+            </label>
+            <input
+              id="userId"
+              name="userId"
+              type="text"
+              placeholder="Enter your user ID"
+              autoComplete="userId"
+              onChange={handleChange}
+              value={formData.userId}
+              required
+              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-xs text-gray-600 uppercase"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+              onChange={handleChange}
+              value={formData.password}
+              required
+              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+            />
+          </div>
+          <SubmitButton>Log In</SubmitButton>
+          <button
+            type="button"
+            onClick={handleGoogleLogin}
+            className="w-full bg-blue-500 text-white py-2 rounded-md"
+          >
+            Login with Google
+          </button>
+
+          {/* 추가 옵션 */}
+          <div className="flex flex-col items-center space-y-4 pt-4">
+            <button
+              onClick={() => router.push(urlConstants.pages.FINDIDBYEMAIL)}
+              className="text-sm font-semibold text-blue-500 hover:text-blue-600 focus:outline-none focus:underline"
+            >
+              Find ID
+            </button>
+            <button
+              onClick={() => router.push(urlConstants.pages.RESETPASSWORD)}
+              className="text-sm font-semibold text-blue-500 hover:text-blue-600 focus:outline-none focus:underline"
+            >
+              Reset Password
+            </button>
+            <p className="text-sm text-gray-600">
+              Don’t have an account?{" "}
+              <Link
+                href={urlConstants.pages.REGISTER}
+                className="font-semibold text-blue-500 hover:text-blue-600 focus:outline-none focus:underline"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </form>
       </div>
-      <div>
-        <label
-          htmlFor="password"
-          className="block text-xs text-gray-600 uppercase"
-        >
-          비밀번호
-        </label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          onChange={handleChange}
-          value={formData.password}
-          required
-          className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-        />
-      </div>
-      {children}
-      <button
-        type="button"
-        onClick={handleGoogleLogin}
-        className="w-full bg-blue-500 text-white py-2 rounded-md"
-      >
-        Login with Google
-      </button>
-    </form>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,16 @@
 "use client";
-import Link from "next/link";
+
 import { LoginForm } from "@/app/component/form/LoginForm";
 import { SubmitButton } from "@/app/component/button/Submit-button";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+
 export default function App() {
   const [queryClient] = useState(() => new QueryClient());
-  const router = useRouter(); // useRouter 훅 사용
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex min-h-screen w-screen items-center justify-center bg-gray-50 px-4">
-        <div className="z-10 w-full max-w-md overflow-hidden rounded-lg border border-gray-300 shadow-lg bg-white">
-          {/* 헤더 */}
-          <div className="flex flex-col items-center justify-center space-y-2 border-b border-gray-200 px-6 py-8 text-center">
-            <h1 className="text-3xl font-extrabold text-gray-900">
-              SURVEY MATE
-            </h1>
-          </div>
-
-          {/* 로그인 폼 */}
-          <div className="px-6 py-6">
-            <LoginForm>
-              <SubmitButton>로그인</SubmitButton>
-            </LoginForm>
-          </div>
-
-          {/* 링크와 추가 옵션 */}
-          <div className="flex flex-col items-center space-y-4 px-6 pb-8">
-            <button
-              onClick={() => router.push(urlConstants.pages.FINDIDBYEMAIL)}
-              className="text-sm font-semibold text-blue-500 hover:text-blue-600 focus:outline-none focus:underline"
-            >
-              아이디 찾기
-            </button>
-            <button
-              onClick={() => router.push(urlConstants.pages.RESETPASSWORD)}
-              className="text-sm font-semibold text-blue-500 hover:text-blue-600 focus:outline-none focus:underline"
-            >
-              비밀번호 재설정
-            </button>
-            <p className="text-sm text-gray-600">
-              계정이 없으신가요?{" "}
-              <Link
-                href={urlConstants.pages.REGISTER}
-                className="font-semibold text-blue-500 hover:text-blue-600 focus:outline-none focus:underline"
-              >
-                회원가입
-              </Link>
-            </p>
-          </div>
-        </div>
-      </div>
+      <LoginForm />
     </QueryClientProvider>
   );
 }
