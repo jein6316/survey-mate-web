8e002398c06111940a3b39ed1d7e896dc08629b5
dajeong <uwhami@gmail.com>
2025-02-13 23:07:25 +0900

    커스텀 훅 수정

---
+++ app/recoil/atoms/userAtom.ts
@@ -0,0 +1,27 @@
+import { atom, selector } from "recoil";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+
+// 사용자 정보 타입 정의
+export interface UserState {
+  isLoggedIn: boolean;
+  userId: string;
+  username: string;
+  role: string;
+  social: string;
+  email: string;
+  exp: number;
+}
+
+export const userAtom = atom<UserState>({
+  key: "userAtom",
+  default: {
+    isLoggedIn: false,
+    userId: "",
+    username: "",
+    role: "guest",
+    social: "homepage",
+    email: "",
+    exp: 0,
+  },
+});
