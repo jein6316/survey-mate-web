05474a78a2408959f41a1401c4ed21aad7635bf8
dajeong <uwhami@gmail.com>
2025-03-27 23:44:30 +0900

    응답 데이터 벡엔드 전송

--- app/api/surveyResponse/SurveyResponseApi.ts
+++ app/api/surveyResponse/SurveyResponseApi.ts
@@ -4,3 +4,8 @@ export const getSurveyForm = async (surveyUrl: string) => {
     const response = await api.get(`/api/survey/response/${surveyUrl}`);
     return response.data;
 };
+
+export const saveSurveyResponse = async ({sqMstId,surveyResponse}: {sqMstId: string, surveyResponse: SurveyResponse[]}) => {
+    const response = await api.post("/api/survey/response/save", {sqMstId, surveyResponse});
+    return response.data;
+};
